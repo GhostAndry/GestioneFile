54824f379e970dfcb32982c721926fec8eca2388
GhostAndry <andrearonzoni20062022@gmail.com>
2025-05-07 10:43:11 +0200

    Changes to be committed: 	modified:   miniupload.sql

--- miniupload.sql
+++ miniupload.sql
@@ -64,18 +64,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-(1,'andrea@ronzoni.it','$2b$10$Ut.tZ6ol15/JwHKm7Fqy1uVtywAf86B49PRmG61MAGYCX1JgzxF/K',1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
